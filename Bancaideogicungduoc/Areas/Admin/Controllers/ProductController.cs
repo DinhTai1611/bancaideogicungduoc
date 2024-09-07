648a76747aeadd85270f95966e807a64894a80e8
DinhTai1611 <dinhtai16112002@gmail.com>
2024-09-07 17:36:28 +0700

    Trang Admin san pham

---
+++ Bancaideogicungduoc/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Bancaideogicungduoc.Reponsitory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bancaideogicungduoc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext Context)
+        {
+            _dataContext = Context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
