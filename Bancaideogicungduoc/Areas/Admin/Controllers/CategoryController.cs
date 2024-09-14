9c4c3fb574ccfae8874668cfa2d755aadb1363d7
DinhTai1611 <dinhtai16112002@gmail.com>
2024-09-14 16:59:43 +0700

    Update code

---
+++ Bancaideogicungduoc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using Bancaideogicungduoc.Models;
+using Bancaideogicungduoc.Reponsitory;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bancaideogicungduoc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CategoryController(DataContext Context)
+        {
+            _dataContext = Context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Loại sản phẩm đã tồn tại");
+                    return View(category);
+                }
+
+                _dataContext.Add(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model dang bi loi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMess = string.Join("\n", errors);
+                return BadRequest(errorMess);
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, CategoryModel category)
+        {
+            var existCategory = _dataContext.Categories.Find(category.Id);
+
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+
+                existCategory.Name = category.Name;
+                existCategory.Description = category.Description;
+                existCategory.Status = category.Status;
+
+                _dataContext.Update(existCategory);
+
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Cập nhật thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model dang bi loi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMess = string.Join("\n", errors);
+                return BadRequest(errorMess);
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+
+            _dataContext.Categories.Remove(category);
+            await _dataContext.SaveChangesAsync();
+            TempData["error"] = "Xóa thành công";
+            return RedirectToAction("Index");
+        }
+    }
+}
