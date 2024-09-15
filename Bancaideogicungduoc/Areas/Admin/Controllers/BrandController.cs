5e091bc7a56cad5a00002ce83602b8ef75ff7ed4
DinhTai1611 <dinhtai16112002@gmail.com>
2024-09-15 16:12:20 +0700

    update code

---
+++ Bancaideogicungduoc/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,111 @@
+﻿using Bancaideogicungduoc.Models;
+using Bancaideogicungduoc.Reponsitory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bancaideogicungduoc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public BrandController(DataContext Context)
+        {
+            _dataContext = Context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Brands.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BrandModel brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == brand.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Thương hiệu đã tồn tại");
+                    return View(brand);
+                }
+
+                _dataContext.Add(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model dang bi loi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMess = string.Join("\n", errors);
+                return BadRequest(errorMess);
+            }
+            return View(brand);
+        }
+        public async Task<IActionResult> Edit(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+            return View(brand);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, BrandModel brand)
+        {
+            var existBrand = _dataContext.Brands.Find(brand.Id);
+
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+
+                existBrand.Name = brand.Name;
+                existBrand.Description = brand.Description;
+                existBrand.Status = brand.Status;
+
+                _dataContext.Update(existBrand);
+
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Cập nhật thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model dang bi loi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMess = string.Join("\n", errors);
+                return BadRequest(errorMess);
+            }
+            return View(brand);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+
+            _dataContext.Brands.Remove(brand);
+            await _dataContext.SaveChangesAsync();
+            TempData["error"] = "Xóa thành công";
+            return RedirectToAction("Index");
+        }
+    }
+}
